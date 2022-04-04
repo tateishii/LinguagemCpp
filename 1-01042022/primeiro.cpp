04f6d037def7d3a077a78e4b331fc64d94e49d7e
Pedro Tateishi <pehen10@gmail.com>
2022-04-04 08:52:32 -0300

    Estudo C++

---
+++ 1-01042022/primeiro.cpp
@@ -0,0 +1,16 @@
+/*
+Programa para exibir uma mensagem. O código foi escrito em C++
+*/
+/*
+Importação da biblioteca de entraa e saída de dados
+*/
+#include <iostream>
+
+int main(){
+
+    //saida padrão de dados 
+    std::cout << "bem vindo ao C++\n";
+
+    return 0;
+
+}
