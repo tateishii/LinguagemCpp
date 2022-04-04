c013c9964842c04543dbba8e160a4d0ea6bea794
Pedro Tateishi <pehen10@gmail.com>
2022-04-04 11:08:40 -0300

    Criaçao da classe usuario

---
+++ 2-04042022/usuario.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::string;
+
+class Usuario{
+
+    public:
+        string nome;
+        string senha;
+        string email;
+        string nivel;
+}
